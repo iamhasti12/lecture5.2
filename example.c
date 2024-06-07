b13802d4ec7ffe6a6f4a6bb0300ae181066a691e
iamhasti12 <vaishnavhasti68@gmail.com>
2024-06-07 15:30:09 +0530

    Add files via upload

---
+++ example.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+
+main()
+{
+	int a,b,c,d;  
+	
+	printf("Enter Value A =");
+	scanf("%d",&a);
+	printf("Enter Value B =");
+	scanf("%d",&b);
+	printf("Enter Value C =");
+	scanf("%d",&c);
+	printf("Enter Value D =");
+	scanf("%d",&d);
+	if (a>b)
+	{
+		if(a>c)
+		{
+			if(a>d)
+	  		{
+	  		  printf("A is max");
+	  		}
+	        else
+	  		{
+	  		  printf(" D is max");
+	  		}
+		}
+	    else
+	    {
+	  	  if(c>d)
+	  	  {
+	  	  	printf("C is max");
+		  }
+		  else
+		  {
+		  	printf("D is max");
+		  }
+	   }
+ }
+	  else
+	  {
+	  	if(b>c)
+	  	{
+	  		if(b>d)
+	  		{
+	  			printf("B is max");
+			  }
+			  else
+			  {
+			   printf("D is max");
+			  }
+		  }
+		  else
+		  {
+		  
+		  if(c>d)
+		  {
+		  	printf("C is max");
+		  }
+		  else 
+		  {
+		  	printf("D is max");        
+		  }
+	  }
+  } 
+
+}
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
